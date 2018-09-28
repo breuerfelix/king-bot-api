ae9e3c646e663ada0a0dc06b5ea0ab5c04f18618
Felix Breuer <f.breuer94@gmail.com>
2018-09-29 01:36:48 +0200

    dev server adjustments

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,21 @@
 import axios, { AxiosInstance } from 'axios';
 import login_to_gameworld from './login';
+import settings from './settings';
 
 class api {
 	ax: AxiosInstance;
-	session: string = '';
-	token: string = '';
-	msid: string = '';
+
+	// set default so dev server can replace later
+	session: string = 'insert_session';
+	token: string = 'insert_token';
+	msid: string = 'insert_msid';
 
 	constructor() {
 		this.ax = axios.create();
 		this.ax.defaults.withCredentials = true;
+
+		//set base url to localhost if in dev mode
+		if(settings.dev_mode) this.ax.defaults.baseURL = 'http://localhost:3030/api';
 	}
 
 	async login(email: string, password: string, gameworld: string) {
@@ -20,7 +26,7 @@ class api {
 		this.token = rv.token;
 		this.msid = rv.msid;
 
-		// set baseURL
+		// set base url
 		this.ax.defaults.baseURL = `https://${gameworld.toLowerCase()}.kingdoms.com/api`;
 	}
 
@@ -36,6 +42,16 @@ class api {
 
 		let response = await this.ax.post(`/?c=player&a=getAll&t${Date.now()}`, payload);
 		console.log(response.data);
+	}
+
+	// for dev server
+	async post(url: string, payload: object) {
+		return await this.ax.post(url, payload);
+	}
+
+	// for dev server
+	async get(url: string) {
+		return await this.ax.get(url);
 	}
 }
 
