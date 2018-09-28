c7ab0e9a1c9564e93059905095d35cefe23bf57b
Felix Breuer <f.breuer94@gmail.com>
2018-09-28 12:17:12 +0200

    added getAll

--- src/api.js
+++ src/api.js
@@ -11,7 +11,28 @@ class api {
 
 	async login(email, password, gameworld){
 		let rv = await login_to_gameworld(this.ax, email, password, gameworld);
-		console.log(rv)
+
+		// assign login credentials
+		this.session = rv.session;
+		this.token = rv.token;
+		this.msid = rv.msid;
+
+		// set baseURL
+		this.ax.defaults.baseURL = `https://${gameworld.toLowerCase()}.kingdoms.com/api`;
+	}
+
+	async get_all(){
+		let session = this.session;
+
+		const payload = {
+			controller: 'player',
+			action: 'getAll',
+			params: {},
+			session
+		};
+
+		let response = await this.ax.post(`/?c=player&a=getAll&t${Date.now()}`, payload);
+		console.log(response.data)
 	}
 }
 
