d719f57abc208fed8e79b088880b69271df79d46
Felix Breuer <f.breuer94@gmail.com>
2018-09-30 20:09:47 +0200

    error handling cred file

--- src/settings.ts
+++ src/settings.ts
@@ -9,7 +9,11 @@ class settings{
 	}
 
 	read_credentials(){
-		let cred: string = fs.readFileSync('./cred.txt', 'utf-8');
+		const filename: string = './cred.txt';
+
+		if(!fs.existsSync(filename)) return null;
+
+		let cred: string = fs.readFileSync(filename, 'utf-8');
 		let cred_array: string[] = cred.trim().split(';');
 
 		return { email: cred_array[0], password: cred_array[1] };
