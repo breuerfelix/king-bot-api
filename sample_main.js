8d09d7adc312690f45fa7ce50ca3d2f428f87b87
Felix Breuer <f.breuer94@gmail.com>
2018-10-15 19:52:39 +0200

    updated to new features

--- sample_main.js
+++ sample_main.js
@@ -1,6 +1,6 @@
 import kingbot from './dist';
 
-const gameworld = 'com5';
+const gameworld = 'test';
 const email = 'your_email@mail.com';
 const password = 'really_save_password';
 
@@ -9,7 +9,13 @@ async function main(){
 	await kingbot.init_data();
 
 	// place bot action below
+	kingbot.finish_earlier();
+
 	kingbot.start_farming([ 'Startup farm list', 'rocking farms' ], '-02- Rome', 600);
+
+	kingbot.add_building_queue({ crop: [4, 4, 3], iron: [5] }, '-01-');
+	kingbot.add_building_queue({ crop: [6, 5, 5, 4], clay: [5, 4] }, '-01-');
+	kingbot.add_building_queue({ clay: [10, 6], wood: [7], iron: [6] }, '-01-');
 }
 
 main();
