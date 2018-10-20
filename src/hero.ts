3036428fd6d543ce3fae3f44ede8bc6918cf84e2
Felix Breuer <f.breuer94@gmail.com>
2018-10-21 01:49:34 +0200

    fix indent, add min health

--- src/hero.ts
+++ src/hero.ts
@@ -1,30 +1,39 @@
-import { Ihero } from './interfaces';
-import { log, find_state_data } from './util';
+import { Ihero, Iplayer } from './interfaces';
+import { log, find_state_data, get_date, get_diff_time } from './util';
 import { sleep } from './util';
 import api from './api';
 import village from './village';
+import player from './player';
 
 class hero {
-  hero_ident: string = 'Hero:';
+	hero_ident: string = 'Hero:';
 
-  async auto_adventure(type: number): Promise<void> {
-    // to get player id
-    const villages_data: any = await village.get_own();
-    const player_id: string = find_state_data(village.own_villages_ident, villages_data)[0].data.playerId;
+	async auto_adventure(type: adventure_type, min_health: number): Promise<void> {
+		const player_data: Iplayer = await player.get();
 
-    while(true) {
-      // to get hero data
-      let response: any[] = await api.get_cache([this.hero_ident + player_id]);
-      const hero: Ihero = find_state_data(this.hero_ident + player_id, response);
+		while(true) {
+			// get hero data
+			const response: any[] = await api.get_cache([ this.hero_ident + player_data.playerId]);
+			const hero: Ihero = find_state_data(this.hero_ident + player_data.playerId, response);
 
-      if (hero.adventurePoints > 0 && !hero.isMoving && hero.status == 0){
-        const res = await api.adventure(type);
-        log(`sent hero on small adventure`);
-      }
+			if (hero.adventurePoints > 0 && !hero.isMoving && hero.status == 0 && Number(hero.health) > min_health){
+				await api.start_adventure(type);
+				log('sent hero on adventure');
+			}
 
-      await sleep(60);
-    }
-  }
+			const diff_time: number = get_diff_time(hero.untilTime);
+			let sleep_time: number = 60;
+
+			if(diff_time > 0) sleep_time = diff_time + 5;
+
+			await sleep(sleep_time);
+		}
+	}
+}
+
+export enum adventure_type {
+	short = 0,
+	long = 1
 }
 
 export default new hero();
