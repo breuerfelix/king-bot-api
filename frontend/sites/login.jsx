0255bab38983f2fe9971e4547f06b87fa2b8dd2b
Felix Breuer <f.breuer94@gmail.com>
2018-10-21 14:49:26 +0200

    added material design

--- frontend/sites/login.jsx
+++ frontend/sites/login.jsx
@@ -3,8 +3,15 @@ import { h, render, Component } from 'preact';
 class Login extends Component {
 	render() {
 		return (
-			<div>
-				loginworld
+			<div class="mdl-grid">
+				<div class="mdl-layout-spacer"></div>
+				<div class="mdl-cell mdl-cell--4-col">
+					This div is centered
+					<button class="mdl-button mdl-js-button mdl-button--raised">
+						Button
+					</button>
+				</div>
+				<div class="mdl-layout-spacer"></div>
 			</div>
 		);
 	}
