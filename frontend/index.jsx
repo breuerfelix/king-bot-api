a1e0e0abb71c1b2958aa390071df93859286b45d
Felix Breuer <f.breuer94@gmail.com>
2018-10-21 13:38:58 +0200

    added basic sites

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,12 +1,25 @@
+// import material lite design
+import 'material-design-lite/material.min.css';
+import 'material-design-lite/material.min.js';
+
 import { h, render, Component } from 'preact';
 
-class Clock extends Component {
-    render() {
-        let time = new Date().toLocaleTimeString();
-        return <span>test</span>;
-    }
+import Router from 'preact-router';
+import Home from './sites/home';
+import Login from './sites/login';
+
+class App extends Component {
+	render() {
+		let time = new Date().toLocaleTimeString();
+		return (
+			<Router>
+				<Home path='/' />
+				<Login path='/login' />
+			</Router>
+		);
+	}
 }
 
 // render an instance of Clock into <body>:
-render(<Clock />, document.body);
+render(<App />, document.body);
 
---
+++ frontend/sites/home.jsx
@@ -0,0 +1,13 @@
+import { h, render, Component } from 'preact';
+
+class Home extends Component {
+	render() {
+		return (
+			<div>
+				hello world
+			</div>
+		);
+	}
+}
+
+export default Home;
