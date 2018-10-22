751604bceac80b6bf61f09779f7d94cec31b84d5
Felix Breuer <f.breuer94@gmail.com>
2018-10-22 13:41:47 +0200

    font awesome icons

--- frontend/components/feature.jsx
+++ frontend/components/feature.jsx
@@ -16,11 +16,16 @@ export default class Feature extends Component {
 
 	toggle_feature = (e) => {
 		this.setState({
-			name: 'est'
+			running: !this.state.running
 		});
 	}
 
 	render() {
+		var toggle_feature = classNames({
+			fas: true,
+			'fa-toggle-on': this.state.running,
+			'fa-toggle-off': !this.state.running
+		});
 		return (
 			<div class="level has-background-white-ter">
 				<div className="level-item" style='padding: 1rem'>
@@ -31,11 +36,22 @@ export default class Feature extends Component {
 				<div className="level-item">
 					{ this.state.description }
 				</div>
-				<div className="level-item" onClick={ this.toggle_feature }>
+				<div className="level-item" >
 					{ this.state.status }
 				</div>
 				<div className="level-item">
-					<Icon />
+					<a class="button is-light is-medium" onClick={ this.toggle_feature }>
+						<span class="icon">
+							<i class={ toggle_feature }></i>
+						</span>
+					</a>
+				</div>
+				<div className="level-item">
+					<a class="button is-light">
+						<span class="icon">
+							<i class="fas fa-edit"></i>
+						</span>
+					</a>
 				</div>
 			</div>
 		);
