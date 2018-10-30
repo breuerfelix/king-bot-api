cecbdf489c28c69a078e86cf2a5480cd61cebef4
Felix Breuer <f.breuer94@gmail.com>
2018-10-30 15:39:58 +0100

    read in buildings.json

--- src/settings.ts
+++ src/settings.ts
@@ -7,9 +7,11 @@ class settings {
 
 	assets_folder: string = BASE_DIR + './assets';
 	database_name: string = '/database.json';
+	buildings_name: string = '/buildings.json';
+	credentials_name: string = '/cred.txt';
 
 	read_credentials(){
-		const filename: string = this.assets_folder + '/cred.txt';
+		const filename: string = this.assets_folder + this.credentials_name;
 
 		if(!fs.existsSync(filename)) return null;
 
@@ -17,6 +19,15 @@ class settings {
 		let cred_array: string[] = cred.trim().split(';');
 
 		return { email: cred_array[0], password: cred_array[1], gameworld: cred_array[2] };
+	}
+
+	get_buildings(): any {
+		const filename: string = this.assets_folder + this.buildings_name;
+
+		if(!fs.existsSync(filename)) return null;
+
+		let data: string = fs.readFileSync(filename, 'utf-8');
+		return JSON.parse(data);
 	}
 }
 
