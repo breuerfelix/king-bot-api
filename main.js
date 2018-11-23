5bee21f62c072a1ec65ccf8d3a19cf238d0e00e4
Felix Breuer <f.breuer94@gmail.com>
2018-11-23 11:36:56 +0100

    removed command line mode

--- main.js
+++ main.js
@@ -6,20 +6,10 @@ const password = '';
 const port = 3000;
 
 async function main(){
-	await kingbot.login(gameworld, email, password);
-
-	// place bot action below
+	kingbot.start_server(gameworld, email, password, port);
 }
 
-async function scout() {
-	await kingbot.login(gameworld, email, password);
-
-	// farmlists which should be send when using npm run scout
-}
-
-if(process.argv[2] == '--scout') scout();
-else if(process.argv[2] == '--gui') kingbot.start_server(gameworld, email, password, port);
+if(process.argv[2] == '--gui') kingbot.start_server(gameworld, email, password, port);
 else main();
 
 export {};
-
