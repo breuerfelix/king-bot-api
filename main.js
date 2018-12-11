a00bf0a690150b69ff2aa89fa793d6dbb7dc8170
Max Foltz <Thomas.M.Foltz@erdc.dren.mil>
2018-12-11 12:12:04 -0600

    Added commented out api call

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 import kingbot from './src';
 
-const gameworld = 'COM5';
-const email = 'tmfoltz@gmail.com';
-const password = 'Swimming168';
+const gameworld = '';
+const email = '';
+const password = '';
 const port = 3000;
 
-async function main(){
-	kingbot.start_server(gameworld, email, password, port);
+async function main() {
+  kingbot.start_server(gameworld, email, password, port);
 }
 
-if(process.argv[2] == '--gui') kingbot.start_server(gameworld, email, password, port);
+if (process.argv[2] == '--gui') kingbot.start_server(gameworld, email, password, port);
 else main();
 
-export {};
+export { };
