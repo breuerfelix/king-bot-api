3628516a7dddd63fef8873d7af06dc05b4c6c0e5
Felix Breuer <f.breuer94@gmail.com>
2018-12-14 19:53:26 +0100

    change docker to also mount database file

--- docker.sh
+++ docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Ask the user for container name and port
 
-echo Hello, what shall i call the container?
+echo hello, what should the container be called?
 read container
 
-echo What port you want to run $container on?
+echo what port you want to run $container on?
 read port
 
-echo what is the name of the cred file?
+echo at which folder do you want to mount the datastorage? for credentials and database.
 read cred
 
-docker container run -d -p $port:3000 -v $PWD/$cred:/usr/kingbot/assets/cred.txt --name $container --restart=always scriptworld/king-bot-api
+sudo docker container run -d -p $port:3000 -v $cred:/usr/kingbot/assets --name $container --restart=always scriptworld/king-bot-api
 
