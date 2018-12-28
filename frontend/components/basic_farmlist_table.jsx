2ace52d6d8a9eefe4b84d958913dc923d1af0f12
Max Foltz <Thomas.M.Foltz@erdc.dren.mil>
2018-12-28 16:36:14 -0600

    Wont attack based on previous reports.

--- frontend/components/basic_farmlist_table.jsx
+++ frontend/components/basic_farmlist_table.jsx
@@ -25,7 +25,7 @@ export default class BasicFarmlistTable extends Component {
 							<th style={row_style}>tribe</th>
 							<th style={row_style}>unit type</th>
 							<th style={row_style}>unit number</th>
-              <th style={row_style}>priority</th>
+							<th style={row_style}>priority</th>
 							<th />
 						</tr>
 					</thead>
@@ -41,17 +41,17 @@ export default class BasicFarmlistTable extends Component {
 class Farm extends Component {
 	state = {
 		toggled: false,
-    distance: null,
-    player_name: null,
-    village_name: null,
-    population: null,
-    x: null,
-    y: null,
-    tribeId: null, 
-    kingdom_tag: null, 
-    unit_number: 2, 
-    unit_type: 0,
-    priority: 10
+		distance: null,
+		player_name: null,
+		village_name: null,
+		population: null,
+		x: null,
+		y: null,
+		tribeId: null,
+		kingdom_tag: null,
+		unit_number: 2,
+		unit_type: 0,
+		priority: 10
 	}
 
 	tribe_dict = {
@@ -63,7 +63,7 @@ class Farm extends Component {
 	render({ content, clicked, unitChanged }, { toggled }) {
 		let { distance, player_name, village_name, population, x, y, tribeId, kingdom_tag, unit_number, unit_type, priority } = content;
 
-    this.state = content;
+		this.state = content;
 
 		distance = Number(distance).toFixed(1);
 		const coordinates = `( ${x} | ${y} )`;
@@ -71,14 +71,15 @@ class Farm extends Component {
 
 		const row_style = {
 			verticalAlign: 'middle',
-      textAlign: 'center',
-      color: color()
-    };
-    
-    function color(){
-      if(priority == 0 || unit_number == 0 || unit_type == 0) return "#FF0000";
-      return '#000000'
-    }
+			textAlign: 'center',
+			color: color()
+		};
+
+		function color() {
+			if (priority == 0 || unit_number == 0 || unit_type == 0) return "#FF0000";
+			if (priority == -1) return "#FFA500"
+			return '#000000'
+		}
 
 		const icon = classNames({
 			'fas': true,
@@ -114,32 +115,32 @@ class Farm extends Component {
 					<input
 						type="text"
 						value={unit_type}
-            placeholder="0"
+						placeholder="0"
 						onChange={async e => {
-              this.setState({ unit_type: e.target.value });
-              await unitChanged(this.state)
+							this.setState({ unit_type: e.target.value });
+							await unitChanged(this.state)
 						}}
 					/>
 				</td>
 				<td style={row_style}>
-        <input
+					<input
 						type="text"
-            value={unit_number}
-            placeholder="2"
+						value={unit_number}
+						placeholder="2"
 						onChange={async e => {
-              this.setState({ unit_number: e.target.value });
-              await unitChanged(this.state)
+							this.setState({ unit_number: e.target.value });
+							await unitChanged(this.state)
 						}}
 					/>
 				</td>
-        <td style={row_style}>
-        <input
+				<td style={row_style}>
+					<input
 						type="text"
-            value={priority}
-            placeholder="10"
+						value={priority}
+						placeholder="10"
 						onChange={async e => {
-              this.setState({ priority: e.target.value });
-              await unitChanged(this.state)
+							this.setState({ priority: e.target.value });
+							await unitChanged(this.state)
 						}}
 					/>
 				</td>
@@ -152,7 +153,7 @@ class Farm extends Component {
 						</span>
 					</a>
 				</td>
-        
+
 			</tr>
 		);
 	}
