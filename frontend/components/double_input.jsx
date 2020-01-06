a8cf78174da32dcc48c011d63664bdd1076ec022
Felix Breuer <f.breuer94@gmail.com>
2020-01-06 22:49:02 +0100

    refactor double input

--- frontend/components/double_input.jsx
+++ frontend/components/double_input.jsx
@@ -1,27 +1,27 @@
-import { h, render, Component } from 'preact';
+import { h } from 'preact';
 
-export default class DoubleInput extends Component {
-	render({ label, placeholder1, placeholder2, value1, value2, onChange1, onChange2 }) {
-		return (
-			<div style='margin-top: 1rem; margin-bottom: 1rem'>
-				<label class="label">{ label }</label>
-				<input
-					class="input is-radiusless"
-					type="text"
-					style="width: 120px;margin-right: 10px;"
-					placeholder={ placeholder1 }
-					value={ value1 }
-					onChange={ onChange1 }
-				/>
-				<input
-					class="input is-radiusless"
-					type="text"
-					style="width: 120px;"
-					placeholder={ placeholder2 }
-					value={ value2 }
-					onChange={ onChange2 }
-				/>
-			</div>
-		);
-	}
-}
+export default ({
+	label, placeholder1, placeholder2,
+	value1, value2, onChange1, onChange2,
+	width = '120px',
+}) => (
+	<div style={{ margin: '1rem 0' }}>
+		<label class='label'>{label}</label>
+		<input
+			class='input is-radiusless'
+			type='text'
+			style={{ width, marginRight: '10px' }}
+			placeholder={ placeholder1 }
+			value={ value1 }
+			onChange={ onChange1 }
+		/>
+		<input
+			class='input is-radiusless'
+			type='text'
+			style={{ width }}
+			placeholder={ placeholder2 }
+			value={ value2 }
+			onChange={ onChange2 }
+		/>
+	</div>
+);
