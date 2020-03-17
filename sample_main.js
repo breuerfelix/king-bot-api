e0f0ec3808006e9ac5af87b4e32d3dcaeb15de2d
Felix Breuer <f.breuer94@gmail.com>
2020-03-17 20:12:01 +0100

    Add proxy support
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>

--- sample_main.js
+++ sample_main.js
@@ -7,5 +7,6 @@ const password = 'verysecret123';
 const sitter_type = ''; // 'sitter' or 'dual'
 const sitter_name = ''; // ingame avatar name
 const port = 3000;
+const proxy = 'http://user:password@127.0.0.1:1234';
 
-kingbot.start_server(gameworld, email, password, sitter_type, sitter_name, port);
+kingbot.start_server(gameworld, email, password, sitter_type, sitter_name, port, proxy);
